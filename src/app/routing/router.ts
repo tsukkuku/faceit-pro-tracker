ca181ed2e4e711376b7f975ee9741e2319c5c083
tsukkuku <bebracs@bk.ru>
2025-06-25 22:12:45 +0300

    feat: add routes for pages

---
+++ src/app/routing/router.ts
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import { ROUTES } from "@/shared/config/routes";
+import { Layout } from "../layout";
+import { HomePage } from "@/pages/home/HomePage";
+import { PlayerPage } from "@/pages/player-page/PlayerPage";
+
+export const route = createBrowserRouter([
+  {
+    path: ROUTES.HOME,
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: HomePage,
+      },
+      {
+        path: ROUTES.PLAYERPAGE,
+        Component: PlayerPage,
+      },
+    ],
+  },
+]);
