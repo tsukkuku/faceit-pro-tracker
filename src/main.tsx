baf6815cd9af069fdfe60765315da1d283b16550
tsukkuku <bebracs@bk.ru>
2025-06-25 22:22:13 +0300

    refactor: change paths to components

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./app/store.ts";
+import { store } from "./app/store/store.ts";
+import App from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
