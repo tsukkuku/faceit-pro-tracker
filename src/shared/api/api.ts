c3292c54aa28438f9916b2cb41ad27f91f2128da
tsukkuku <bebracs@bk.ru>
2025-06-27 19:48:41 +0300

    refactor: change import to correct

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,4 @@
-import { fetchBaseQuery } from "@reduxjs/toolkit/query";
-import { createApi } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
