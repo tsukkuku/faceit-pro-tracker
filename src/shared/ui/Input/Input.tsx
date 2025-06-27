c1f360959989a7c157492f2d1bc4afad8c8988c3
tsukkuku <bebracs@bk.ru>
2025-06-28 00:51:28 +0300

    feat: add variants for Input component

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,8 +1,19 @@
 import type { FC, InputHTMLAttributes } from "react";
 import style from "./Input.module.scss";
+import clsx from "clsx";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+type InputVariants = "primary" | "secondary";
 
-export const Input: FC<InputProps> = ({ ...props }) => {
-  return <input type="text" className={style.customInput} {...props} />;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  variant?: InputVariants;
+}
+
+export const Input: FC<InputProps> = ({
+  variant = "primary",
+  className,
+  ...props
+}) => {
+  return (
+    <input type="text" className={clsx(style[variant], className)} {...props} />
+  );
 };
