8c5dcd4aaf8ce7ff0776efa2e41717f6799d437e
tsukkuku <bebracs@bk.ru>
2025-06-28 00:46:33 +0300

    refactor: moved layout to folder

--- src/app/layout/Layout/Layout.tsx
+++ src/app/layout/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "./Header";
+import { Header } from "../Header";
+import style from "./Layout.module.scss";
 
 export const Layout = () => {
   return (
     <>
       <Header />
-      <main style={{ marginTop: "40px" }}>
+      <main className={style.mainContent}>
         <Outlet />
       </main>
     </>
