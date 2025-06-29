fe9c0c6d71514d14ef6173c29a0baa79367a3696
tsukkuku <bebracs@bk.ru>
2025-06-29 21:49:56 +0300

    refactor: delete getLocalStorage from public api

--- src/modules/theme/index.ts
+++ src/modules/theme/index.ts
@@ -1,6 +1,5 @@
 import { themeSlice } from "./slices/slice";
 
-export { getLocalStorage } from "./helpers/getLocalStorage";
 export { toggleTheme } from "./slices/slice";
 export { ThemeProvider } from "./components/ThemeProvider/ThemeProvider";
 export { ThemeToggle } from "./components/ThemeToggle/ThemeToggle";
