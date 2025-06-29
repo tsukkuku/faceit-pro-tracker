09b0a24819a4a9b4caa2dc470c6f47c0315f7ccd
tsukkuku <bebracs@bk.ru>
2025-06-29 21:23:45 +0300

    feat: add ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store/store.ts";
+import { ThemeProvider } from "./modules/theme/index.ts";
 import App from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
