382651a3a3e0b156cd3d367dba9bbccc5ee426d2
tsukkuku <bebracs@bk.ru>
2025-07-02 18:18:31 +0300

    refactor: change variable name and new svg export

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,3 +1,5 @@
-import VERIFY_SVG from "./svg/fuse_foundations_iconsColoured_special_goldenVerified 1.svg";
+import VERIFY_SVG from "./svg/fuse_foundations_iconsColoured_special_goldenVerified 1.svg?react";
+import rank_svg from "./svg/fuse_foundations_icons_rank_challengerIcon.svg?react";
 
-export const svg = VERIFY_SVG;
+export const VerifySVG = VERIFY_SVG;
+export const RankSVG = rank_svg;
