59c9f0197587083fab22f40a8bfb35de13d4d80f
tsukkuku <bebracs@bk.ru>
2025-07-02 18:20:40 +0300

    refactor: change svg name

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,5 +1,5 @@
 import VERIFY_SVG from "./svg/fuse_foundations_iconsColoured_special_goldenVerified 1.svg?react";
-import rank_svg from "./svg/fuse_foundations_icons_rank_challengerIcon.svg?react";
+import RANK_SVG from "./svg/fuse_foundations_icons_rank_challengerIcon.svg?react";
 
 export const VerifySVG = VERIFY_SVG;
-export const RankSVG = rank_svg;
+export const RankSVG = RANK_SVG;
