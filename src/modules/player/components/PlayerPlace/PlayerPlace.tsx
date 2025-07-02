9cb01a106561ac3062c3b5192f7e4f2bd7d6bf27
tsukkuku <bebracs@bk.ru>
2025-07-02 18:19:30 +0300

    feat: add new component PlayerPlace

---
+++ src/modules/player/components/PlayerPlace/PlayerPlace.tsx
@@ -0,0 +1,26 @@
+import type { FC, ReactNode } from "react";
+import { Button } from "@/shared/ui";
+import { RankSVG } from "@/shared/assets";
+import clsx from "clsx";
+import style from "./PlayerPlace.module.scss";
+
+export type PlayerPlaceVariants = "top1" | "top2" | "top3" | "default";
+
+interface PlayerPlaceProps {
+  children?: ReactNode;
+  variant?: PlayerPlaceVariants;
+}
+
+export const PlayerPlace: FC<PlayerPlaceProps> = ({
+  children,
+  variant = "default",
+}) => {
+  return (
+    <>
+      <Button variant="player-place" className={clsx(style[variant])}>
+        {children}
+        <RankSVG width={30} height={30} className={style.rankIcon} />
+      </Button>
+    </>
+  );
+};
