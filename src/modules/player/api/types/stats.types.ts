ddb26f9aa030539eaa203f2daec760e89babec17
tsukkuku <bebracs@bk.ru>
2025-07-05 20:58:44 +0300

    refactor: recentResult property type fixed

--- src/modules/player/api/types/stats.types.ts
+++ src/modules/player/api/types/stats.types.ts
@@ -5,12 +5,6 @@ export interface PlayerStats {
     avgHeadshots: string;
     longestWinStreak: string;
     winrate: string;
-    recentResults: {
-      0: string;
-      1: string;
-      2: string;
-      3: string;
-      4: string;
-    };
+    recentResults: string[]
   };
 }
