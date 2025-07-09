646c228b12454c2d757497b6a49fce67e44b0d43
tsukkuku <bebracs@bk.ru>
2025-07-09 18:46:08 +0300

    fix: fixed path to PlayerLevel component

--- src/modules/player/components/profile/PlayerStats/SkillLevel/SkillLevel.tsx
+++ src/modules/player/components/profile/PlayerStats/SkillLevel/SkillLevel.tsx
@@ -1,8 +1,8 @@
 import type { FC } from "react";
 import { ProgressBar } from "../ProgressBar";
 import { EloInfo } from "../EloInfo";
+import { PlayerLevel } from "@/shared/ui";
 import style from "./SkillLevel.module.scss";
-import { PlayerLevel } from "@/shared/ui/PlayerLevel/PlayerLevel";
 
 interface SkillLevelProps {
   elo: number;
