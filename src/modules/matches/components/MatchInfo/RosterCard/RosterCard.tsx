53e3df6cd42260fbdd16a2162bbbca6fffcb2401
tsukkuku <bebracs@bk.ru>
2025-07-15 16:02:14 +0300

    refactor: replace useNavigation with Link in RosterCard and update styles RosterCard & RosterSkeleton

--- src/modules/matches/components/MatchInfo/RosterCard/RosterCard.tsx
+++ src/modules/matches/components/MatchInfo/RosterCard/RosterCard.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   useGetPlayerInMatchInfoQuery,
   useGetPlayerPositionQuery,
@@ -17,7 +17,6 @@ interface RosterCardProps {
 }
 
 export const RosterCard: FC<RosterCardProps> = ({ roster }) => {
-  const navigate = useNavigate();
   const { data: player, isLoading } = useGetPlayerInMatchInfoQuery(
     roster.player_id
   );
@@ -30,48 +29,49 @@ export const RosterCard: FC<RosterCardProps> = ({ roster }) => {
   if (isLoading || isLoadingPosition) return <RosterSkeleton />;
   if (!player?.games.cs2.skill_level) return;
   return (
-    <div
-      className={style.rosterCard}
-      onClick={() => navigate(`/player/${player?.player_id}`)}
-    >
-      <div className={style.avatarContainer}>
-        <Avatar
-          size="md"
-          src={player?.avatar}
-          alt="Player avatar"
-          fallback={NO_AVATAR}
-        />
-      </div>
-      <div className={style.playerNickname}>
-        <div className={style.nicknameContainer}>
-          <div className={style.faceitNickname}>
-            <div className={style.nickname}>{player?.nickname}</div>
-            {player?.verified && <VerifySVG width={16} height={16} />}
-            <span>
-              <ReactCountryFlag
-                countryCode={player?.country.toUpperCase() || ""}
-                svg
-              />
-            </span>
+    <Link className={style.rosterCard} to={`/player/${player?.player_id}`}>
+      <div className={style.playerContainer}>
+        <div className={style.playerInfo}>
+          <div className={style.avatarContainer}>
+            <Avatar
+              size="md"
+              src={player?.avatar}
+              alt="Player avatar"
+              fallback={NO_AVATAR}
+            />
           </div>
-          <div className={style.steamNickname}>
-            {player?.games.cs2.game_player_name}
+          <div className={style.playerNickname}>
+            <div className={style.nicknameContainer}>
+              <div className={style.faceitNickname}>
+                <div className={style.nickname}>{player?.nickname}</div>
+                {player?.verified && <VerifySVG width={16} height={16} />}
+                <span>
+                  <ReactCountryFlag
+                    countryCode={player?.country.toUpperCase() || ""}
+                    svg
+                  />
+                </span>
+              </div>
+              <div className={style.steamNickname}>
+                {player?.games.cs2.game_player_name}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={style.playerTop}>
-        <div className={style.playerElo}>
-          <Elo width={20} height={20} color="gray" />
-          <div className={style.eloValue}>{player?.games.cs2.faceit_elo}</div>
+        <div className={style.playerTop}>
+          <div className={style.playerElo}>
+            <Elo width={20} height={20} color="gray" />
+            <div className={style.eloValue}>{player?.games.cs2.faceit_elo}</div>
+          </div>
+          {position && (
+            <PlayerLevel
+              position={position.position}
+              skill_level={player?.games.cs2.skill_level || 0}
+              className={style.rankImage}
+            />
+          )}
         </div>
-        {position && (
-          <PlayerLevel
-            position={position.position}
-            skill_level={player?.games.cs2.skill_level || 0}
-            className={style.rankImage}
-          />
-        )}
       </div>
-    </div>
+    </Link>
   );
 };
--- src/modules/matches/components/MatchInfo/RosterCard/RosterSkeleton/RosterSkeleton.tsx
+++ src/modules/matches/components/MatchInfo/RosterCard/RosterSkeleton/RosterSkeleton.tsx
@@ -3,14 +3,18 @@ import style from "./RosterSkeleton.module.scss";
 export const RosterSkeleton = () => {
   return (
     <div className={style.rosterSkeleton}>
-      <div className={style.rosterSkeletonAvatar}></div>
-      <div className={style.rosterNickname}>
-        <div className={style.nickname}></div>
-        <div className={style.steamNickname}></div>
-      </div>
-      <div className={style.playerLevel}>
-        <div className={style.playerElo}></div>
-        <div className={style.playerRank}></div>
+      <div className={style.rosterSkeletonContainer}>
+        <div className={style.rosterSkeletonPlayerInfo}>
+          <div className={style.rosterSkeletonAvatar}></div>
+          <div className={style.rosterNickname}>
+            <div className={style.nickname}></div>
+            <div className={style.steamNickname}></div>
+          </div>
+        </div>
+        <div className={style.playerLevel}>
+          <div className={style.playerElo}></div>
+          <div className={style.playerRank}></div>
+        </div>
       </div>
     </div>
   );
