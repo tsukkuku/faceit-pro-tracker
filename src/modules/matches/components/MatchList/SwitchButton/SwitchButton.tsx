31f06280297e490860c984d77247f48508462d60
tsukkuku <bebracs@bk.ru>
2025-07-16 17:59:38 +0300

    refactor: delete unnecessary prop for icon

--- src/modules/matches/components/MatchList/SwitchButton/SwitchButton.tsx
+++ src/modules/matches/components/MatchList/SwitchButton/SwitchButton.tsx
@@ -5,16 +5,16 @@ import { BsFillGrid3X3GapFill } from "react-icons/bs";
 import style from "./SwitchButton.module.scss";
 
 interface SwitchButtonProps {
-	isFlex: boolean
-	setIsFlex: (value: boolean) => void
+  isFlex: boolean;
+  setIsFlex: (value: boolean) => void;
 }
 
-export const SwitchButton: FC<SwitchButtonProps> = ({isFlex, setIsFlex}) => {
+export const SwitchButton: FC<SwitchButtonProps> = ({ isFlex, setIsFlex }) => {
   return (
     <div className={style.switchVariant}>
       <div onClick={() => setIsFlex(true)}>
         <Button className={isFlex ? style.active : style.noActive}>
-          <MdTableRows color={isFlex ? "var(--text-color)" : ""} />
+          <MdTableRows />
         </Button>
       </div>
       <div onClick={() => setIsFlex(false)}>
